fb51f82edb487a6e525e908fd5eb44bd59647544
Peter DeVita <mewtwo2643@yahoo.com>
2019-01-09 16:55:10 -0500

    GifReversingBot Updates

--- core/constants.py
+++ core/constants.py
@@ -3,16 +3,18 @@ from core.credentials import CredentialsLoader
 user_agent = "GifReversingBot v{} by /u/pmdevita"
 spoof_user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:62.0) Gecko/20100101 Firefox/62.0"
 imgur_spoof_cookie = CredentialsLoader.get_credentials()['imgur']['imgur_cookie']
-version = "2.6.7"
+version = "0.5"
 sleep_time = 90
 username = CredentialsLoader.get_credentials()['reddit']['username']
 
 bot_footer = "---\n\n^(I am a bot.) [^(Report an issue)]" \
                  "(https://www.reddit.com/message/compose/?to=pmdevita&subject=GifReversingBot%20Issue)"
 
-nsfw_reply_template = "##NSFW\n\nHere is your gif!\n{}\n\n" + bot_footer
+nsfw_reply_template = "##NSFW\n\n{}\n\n" + bot_footer
 
-reply_template = "Here is your gif!\n{}\n\n" + bot_footer
+# reply_template = "Here is your gif!\n{}\n\n" + bot_footer
+
+reply_template = "{}\n\n" + bot_footer
 
 reply_ban_subject = "Here is your gif!"
 
@@ -26,6 +28,7 @@ unnecessary_manual_message = "\n\nJust so you know, you don't have to manually g
 VIDEO = 1
 GIF = 2
 OTHER = 3
+LINK = 4
 
 GFYCAT = 1
 IMGUR = 2
